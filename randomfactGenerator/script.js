87bd7a9c85824deb91cbbf1dc7a8b6f1affed4eb
ebin <109602426+ebinjosey@users.noreply.github.com>
2025-03-25 11:07:31 +0400

    added loading class

--- randomfactGenerator/script.js
+++ randomfactGenerator/script.js
@@ -2,11 +2,13 @@ const factText = document.querySelector(".fact"),
 factBtn = document.querySelector("button");
 
 function randomFact() {
+    factBtn.classList.add("loading");
     factBtn.innerText = "Loading Fact..."
     fetch("https://uselessfacts.jsph.pl/api/v2/facts/random").then(res => res.json()).then(result =>{
         console.log(result)
         factText.innerText = result.text;
-        factBtn.innerText = "New Fact"
+        factBtn.innerText = "New Fact";
+        factBtn.classList.remove("loading");
     });
 }
 
