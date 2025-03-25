aa255935fff052b0236452513d63449573561eb7
ebin <109602426+ebinjosey@users.noreply.github.com>
2025-03-25 11:01:06 +0400

    randomFact Function

--- randomfactGenerator/script.js
+++ randomfactGenerator/script.js
@@ -1,1 +1,8 @@
+const factText = document.querySelector(".fact"),
+factBtn = document.querySelector("button");
 
+function randomFact() {
+    fetch("https://uselessfacts.jsph.pl/api/v2/facts/random").then(res => res.json())
+}
+
+factBtn.addEventListener("click", randomFact);
